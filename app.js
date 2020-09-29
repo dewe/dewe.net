ac347c67d42b687f8742b37d94a5db4796fcd17e
Johan Dewe <johan@dewe.net>
2020-09-29 22:36:43 +0200

    Font fix

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.use(express.static('public', {
   maxAge: 7200000
 }))
 
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry can't find that!")
+})
+
 app.get('/easify', (req, res) => { res.redirect('/framsteget'); });
 app.get('/dropjaw', (req, res) => { res.redirect('/framsteget'); });
 
